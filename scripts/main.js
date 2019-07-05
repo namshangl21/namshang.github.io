8ec363710d587d91c15c5f5ca8c0d111947ddb97
namshangl21 <45559162+namshangl21@users.noreply.github.com>
2019-07-05 16:19:59 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/man-utd1.png') {
+      myImage.setAttribute ('src','images/man-utd2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/man-utd1.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Manchester United appreciates you, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
